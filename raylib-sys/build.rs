e4bc437d077472a7feb1f6b1222ee7947272022e
borkin8r <borkin8r@users.noreply.github.com>
2019-03-24 23:39:44 -0500

    Added shell32 lib to link against
    Unresolved external linker errors were occurring because shell32 was removed from the standard library. There for explicit linking is required. More info here: https://github.com/rust-lang/rust/issues/56839

--- raylib-sys/build.rs
+++ raylib-sys/build.rs
@@ -18,6 +18,7 @@ fn main() {
     if cfg!(target_os = "windows") {
         println!("cargo:rustc-link-lib=dylib=gdi32");
         println!("cargo:rustc-link-lib=dylib=user32");
+        println!("cargo:rustc-link-lib=dylib=shell32");
     }
     if cfg!(target_os = "linux") {
         println!("cargo:rustc-link-search=/usr/local/lib");
