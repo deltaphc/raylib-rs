1a389f1a693f6ffa55b347a0e4af42c04ab7a7b5
12ColcloughJ <joshuacolclough2@googlemail.com>
2019-04-12 20:48:03 +0100

    Sorted out workspace stuff.

--- examples/core/basic_window/src/main.rs
+++ examples/core/basic_window/src/main.rs
@@ -1,3 +1,20 @@
+use raylib::Color;
+
 fn main() {
-    println!("Hello, world!");
+	let rl = raylib::init()  					// Initialize the raylib handle
+		.size(800, 450)
+		.title("raylib [core] example - basic window")
+		.build();
+
+	rl.set_target_fps(60);   // Limits fps
+
+	while !rl.window_should_close() {
+		// Update stuff here
+
+		rl.begin_drawing();
+		rl.clear_background(Color::RAYWHITE);
+		// Draw stuff here
+		rl.draw_text("Congrats! You created your first window!", 190, 200, 20, Color::LIGHTGRAY);
+		rl.end_drawing();
+	}
 }
