998af6170daa798df15d8c7bbfe629d86f32594f
Dacode45 <davidrolandayeke@gmail.com>
2020-03-11 00:54:04 -0500

    [General] bullets

--- samples/space_eaters.rs
+++ samples/space_eaters.rs
@@ -3,14 +3,13 @@ use legion::prelude::*;
 use raylib::prelude::*;
 use structopt::StructOpt;
 
-mod options;
-
 const ARENA_WIDTH: i32 = 128;
 const ARENA_HEIGHT: i32 = 128;
 const PIXEL_SCALE: i32 = 4;
 const PS: f32 = PIXEL_SCALE as f32;
 const WINDOW_WIDTH: i32 = ARENA_WIDTH * PIXEL_SCALE;
 const WINDOW_HEIGHT: i32 = ARENA_HEIGHT * PIXEL_SCALE;
+const SQUARE: Rectangle = Rectangle::new(0.0, 0.0, 8.0, 8.0);
 
 trait RectangleEx: std::borrow::Borrow<Rectangle> {
     fn resize(&self, scale: f32) -> Rectangle {
@@ -34,6 +33,23 @@ trait RectangleEx: std::borrow::Borrow<Rectangle> {
         r.x = x;
         r.y = y;
         r
+    }
+
+    fn tl(&self) -> Vector2 {
+        let mut r = self.borrow();
+        vec2(r.x, r.y)
+    }
+    fn tr(&self) -> Vector2 {
+        let mut r = self.borrow();
+        vec2(r.x + r.width, r.y)
+    }
+    fn bl(&self) -> Vector2 {
+        let mut r = self.borrow();
+        vec2(r.x, r.y + r.height)
+    }
+    fn br(&self) -> Vector2 {
+        let mut r = self.borrow();
+        vec2(r.x + r.width, r.y + r.height)
     }
 }
 impl RectangleEx for &Rectangle {}
@@ -65,6 +81,7 @@ struct Sprite(usize);
 struct SpriteIndices {
     player_anim: Animation,
     player_bullet_1: usize,
+    green_enemy_anim: Animation,
 }
 
 #[derive(Clone, Debug, PartialEq)]
@@ -81,9 +98,6 @@ struct Player {
     bullet_speed: f32,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-struct Bullet;
-
 impl Default for Player {
     fn default() -> Player {
         Player {
@@ -91,6 +105,69 @@ impl Default for Player {
             bullet_speed: ARENA_HEIGHT as f32 / 1.0,
         }
     }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum EnemyType {
+    Green,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Enemy {
+    kind: EnemyType,
+    speed: f32,
+    bullet_speed: f32,
+}
+
+impl Enemy {
+    fn green() -> Self {
+        Self {
+            kind: EnemyType::Green,
+            speed: ARENA_HEIGHT as f32 / 10.0,
+            bullet_speed: ARENA_HEIGHT as f32 / 5.0,
+        }
+    }
+
+    fn anim(&self, sprite_indices: &SpriteIndices) -> Animation {
+        use EnemyType::*;
+        match self.kind {
+            Green => sprite_indices.green_enemy_anim.clone(),
+        }
+    }
+}
+
+struct Spawner {
+    timer: f32,
+    next_spawn: f32,
+}
+
+impl Default for Spawner {
+    fn default() -> Self {
+        Self {
+            timer: 0.0,
+            next_spawn: 1.0,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Bullet;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Collider {
+    aabb: Rectangle,
+}
+
+impl Collider {
+    fn new(rec: &Rectangle) -> Self {
+        Self { aabb: rec.clone() }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Bounds {
+    StayIn,
+    Destroy,
 }
 
 fn sprite_extents() -> (Vec<Rectangle>, SpriteIndices) {
@@ -110,6 +187,12 @@ fn sprite_extents() -> (Vec<Rectangle>, SpriteIndices) {
             prog: 0.0,
         },
         player_bullet_1: 32,
+        green_enemy_anim: Animation {
+            frames: vec![3, 3 + 16],
+            current: 3,
+            speed: 5.0,
+            prog: 0.0,
+        },
     };
     (extents, indices)
 }
@@ -120,7 +203,6 @@ fn load_sprite_sheet(rl: &mut RaylibHandle, thread: &RaylibThread) -> Texture2D 
 }
 
 fn main() {
-    let opt = options::Opt::from_args();
     let (mut rl, thread) = raylib::init()
         .size(WINDOW_WIDTH, WINDOW_HEIGHT)
         .title("Space Eaters")
@@ -129,38 +211,58 @@ fn main() {
     rl.set_window_icon(&logo);
     rl.set_target_fps(60);
 
-    let (w, h) = (WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32);
+    let (_w, _h) = (WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32);
     // subtracting 8.0 is a hack so we only have to check the top left corner of most sprites
     // assuming they are 8x8
-    let world_ex = Rectangle::new(
-        0.0,
-        0.0,
-        ARENA_WIDTH as f32 - 8.0,
-        ARENA_HEIGHT as f32 - 8.0,
-    );
+    let world_ex = Rectangle::new(0.0, 0.0, ARENA_WIDTH as f32, ARENA_HEIGHT as f32);
 
     let universe = Universe::new();
     let mut world = universe.create_world();
 
+    let mut spawner = Spawner::default();
+    let mut destroy = Vec::new();
+
     let (s_extents, s_indices) = sprite_extents();
     let s_sheet = load_sprite_sheet(&mut rl, &thread);
 
-    // I know sprite could be a tab, but legion's documentation is so
+    // I know sprite could be a tag, but legion's documentation is so
     // terrible that I have no idea how to use tags right.
     world.insert(
-        (),
+        (Bounds::StayIn,),
         (0..1).map(|_| {
             (
                 Position(vec2(ARENA_WIDTH as f32 / 2.0, ARENA_HEIGHT as f32 / 2.0)),
                 Velocity(Vector2::zero()),
                 s_indices.player_anim.clone(),
                 Player::default(),
+                Collider::new(&SQUARE),
             )
         }),
     );
 
     while !rl.window_should_close() {
         let dt = rl.get_frame_time();
+        spawner.timer += dt;
+
+        // Spawning Logic
+        if spawner.timer > spawner.next_spawn {
+            spawner.timer = 0.0;
+            spawner.next_spawn = std::f32::INFINITY;
+
+            let enemy = Enemy::green();
+            world.insert(
+                (Bounds::Destroy,),
+                (0..1).map(|_| {
+                    (
+                        Position(vec2(ARENA_WIDTH as f32 / 2.0, ARENA_HEIGHT as f32 / 4.0)),
+                        Velocity(vec2(0.0, 1.0) * enemy.speed),
+                        enemy.anim(&s_indices),
+                        enemy,
+                        Collider::new(&SQUARE),
+                    )
+                }),
+            );
+        }
 
         // Player Logic
         let query = <(
@@ -170,7 +272,7 @@ fn main() {
             Write<Player>,
         )>::query();
         let mut player_shoot = None;
-        for (mut pos, mut vel, mut anim, player) in query.iter(&mut world) {
+        for (pos, mut vel, mut anim, player) in query.iter(&mut world) {
             use raylib::consts::KeyboardKey::*;
             // Move stuff
             let right = rl.is_key_down(KEY_D);
@@ -205,11 +307,12 @@ fn main() {
                     Velocity(vec2(0.0, -player.bullet_speed)),
                     Sprite(s_indices.player_bullet_1),
                     Bullet,
+                    Collider::new(&SQUARE),
                 ));
             }
         }
         if let Some(player_shoot) = player_shoot {
-            world.insert((), (0..1).map(|_| player_shoot));
+            world.insert((Bounds::Destroy,), (0..1).map(|_| player_shoot));
         }
 
         // Things that move
@@ -217,8 +320,36 @@ fn main() {
         for (mut pos, vel) in query.iter(&mut world) {
             let old_pos = pos.0;
             pos.0 += vel.0 * dt;
-            if !world_ex.check_collision_point_rec(pos.0) {
-                pos.0 = old_pos;
+        }
+
+        // Make sure player stays in bounds
+        let query = <(Write<Position>, Read<Collider>)>::query().filter(tag_value(&Bounds::StayIn));
+        for (mut pos, col) in query.iter(&mut world) {
+            let at = col.aabb.move_to(pos.x, pos.y);
+            let mut bounds = world_ex.clone();
+            let min_y = bounds.y;
+            let max_y = bounds.br().y - col.aabb.height;
+            bounds.y += col.aabb.height;
+            bounds.height -= 2.0 * col.aabb.height;
+
+            if !bounds.check_collision_recs(&at) {
+                if at.tl().x > bounds.tr().x {
+                    pos.0.x = bounds.x;
+                } else if at.tr().x < bounds.x {
+                    pos.0.x = bounds.tr().x;
+                }
+                pos.0.y = pos.y.max(min_y).min(max_y);
+            }
+        }
+
+        // Destroy stuff out of bounds
+
+        let query =
+            <(Write<Position>, Read<Collider>)>::query().filter(tag_value(&Bounds::Destroy));
+        for (ent, (mut pos, col)) in query.iter_entities(&mut world) {
+            let at = col.aabb.move_to(pos.x, pos.y);
+            if !world_ex.check_collision_recs(&at) {
+                destroy.push(ent);
             }
         }
 
@@ -246,6 +377,11 @@ fn main() {
                 0.0,
                 Color::WHITE,
             );
+        }
+
+        // Cleanup
+        for ent in destroy.drain(..) {
+            world.delete(ent);
         }
 
         // for ext in &s_extents {
