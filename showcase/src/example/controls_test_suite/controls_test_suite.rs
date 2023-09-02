23777629fed290799e4ec1279b05e6aa243fb3cf
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-09-02 05:28:58 +0300

    remove unnecessary heap allocation

--- showcase/src/example/controls_test_suite/controls_test_suite.rs
+++ showcase/src/example/controls_test_suite/controls_test_suite.rs
@@ -115,7 +115,7 @@ pub fn run(rl: &mut RaylibHandle, thread: &RaylibThread) -> crate::SampleOut {
             let droppedFiles = rl.get_dropped_files();
 
             if (droppedFiles.len() > 0) && droppedFiles[0].ends_with(".rgs") {
-                rl.gui_load_style(Some(&CString::new(droppedFiles[0].clone()).unwrap()));
+                rl.gui_load_style(Some(&CString::new(droppedFiles[0].as_bytes()).unwrap()));
             }
 
             rl.clear_dropped_files();
