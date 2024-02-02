9b99e61a20665f2dad5c54d342617a15e3154ffc
IoIxD <alphaproject217@gmail.com>
2024-02-02 15:04:42 -0700

    [tests] test for new file functions

--- raylib-test/src/data.rs
+++ raylib-test/src/data.rs
@@ -13,4 +13,35 @@ mod data_test {
             "./test_out/export_data.txt",
         );
     }
+
+    ray_test!(application_dir);
+    fn application_dir(_: &RaylibThread) {
+        let mut handle = TEST_HANDLE.write().unwrap();
+        let rl = handle.as_mut().unwrap();
+
+        println!(
+            "\n\n=====\nApplication directory is {}\n=====\n\n",
+            rl.application_directory()
+        );
+    }
+
+    ray_test!(file_length_test);
+    fn file_length_test(_: &RaylibThread) {
+        let mut handle = TEST_HANDLE.write().unwrap();
+        let rl = handle.as_mut().unwrap();
+
+        let len = rl.get_file_length("./resources/just_exists.txt").unwrap();
+        assert!(len == 18);
+    }
+
+    ray_test!(is_path_file);
+    fn is_path_file(_: &RaylibThread) {
+        let mut handle = TEST_HANDLE.write().unwrap();
+        let rl = handle.as_mut().unwrap();
+
+        let len = rl.is_path_file("./resources/just_exists.txt").unwrap();
+        assert!(len == true);
+        let len = rl.is_path_file("./resources/").unwrap();
+        assert!(len == false);
+    }
 }
