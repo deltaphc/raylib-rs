bdff18688d9535a2eb77fbcb539f28b306c4e946
IoIxD <alphaproject217@gmail.com>
2024-02-06 16:44:27 -0700

    [general] ran cargo fmt to make the formatter happy

--- raylib/src/core/math.rs
+++ raylib/src/core/math.rs
@@ -203,28 +203,28 @@ impl Vector2 {
         *self / length_sqr.sqrt()
     }
 
-   /// Rotates the vector by `angle` radians.
-   pub fn rotate(&mut self, angle: f32) {
+    /// Rotates the vector by `angle` radians.
+    pub fn rotate(&mut self, angle: f32) {
         let cos_res = angle.cos();
         let sin_res = angle.sin();
 
         let result = Vector2::new(
             self.x * cos_res - self.y * sin_res,
-            self.x * sin_res + self.y * cos_res
+            self.x * sin_res + self.y * cos_res,
         );
 
         self.x = result.x;
         self.y = result.y;
     }
 
-   /// Returns a new `Vector2` rotated by `angle` radians.
-   pub fn rotated(&self, angle: f32) -> Vector2 {
+    /// Returns a new `Vector2` rotated by `angle` radians.
+    pub fn rotated(&self, angle: f32) -> Vector2 {
         let cos_res = angle.cos();
         let sin_res = angle.sin();
 
         Vector2::new(
             self.x * cos_res - self.y * sin_res,
-            self.x * sin_res + self.y * cos_res
+            self.x * sin_res + self.y * cos_res,
         )
     }
 
