ae0e7f05072bcc4c70c27c3a90cf6c9164f9e20e
IoIxD <alphaproject217@gmail.com>
2024-02-06 14:18:17 -0700

    [files] added FilePathList and the functions for creating it.

--- raylib/src/core/file.rs
+++ raylib/src/core/file.rs
@@ -2,7 +2,38 @@
 use crate::ffi;
 
 use crate::core::RaylibHandle;
-use std::ffi::{CStr, CString, NulError};
+use std::{
+    ffi::{CStr, CString, NulError, OsString},
+    iter::FromIterator,
+    ops::Deref,
+};
+
+use std::vec::IntoIter;
+make_thin_wrapper!(FilePathList, ffi::FilePathList, ffi::UnloadDirectoryFiles);
+make_thin_wrapper!(
+    DroppedFilePathList,
+    ffi::FilePathList,
+    ffi::UnloadDroppedFiles
+);
+
+impl FilePathList {
+    /// Length of the file path list
+    pub const fn len(&self) -> u32 {
+        self.0.count
+    }
+    /// The amount of files that can be held in this list.
+    pub const fn capacity(&self) -> u32 {
+        self.0.capacity
+    }
+    /// The paths held in this list.
+    /// This function is NOT constant and the inner array will be copied into the returned Vec every time you call this.
+    pub fn paths(&self) -> Vec<&str> {
+        unsafe { std::slice::from_raw_parts(self.0.paths, self.len() as usize) }
+            .iter()
+            .map(|f| unsafe { CStr::from_ptr(*f) }.to_str().unwrap())
+            .collect()
+    }
+}
 
 impl RaylibHandle {
     /// Checks if a file has been dropped into the window.
@@ -11,22 +42,6 @@ impl RaylibHandle {
         unsafe { ffi::IsFileDropped() }
     }
 
-    /// Gets dropped filenames.
-    pub fn load_dropped_files(&self) -> Vec<String> {
-        let mut v = Vec::new();
-        unsafe {
-            let dropfiles = ffi::LoadDroppedFiles();
-            for i in 0..dropfiles.count {
-                let filestr = CStr::from_ptr(*dropfiles.paths.offset(i as isize))
-                    .to_str()
-                    .unwrap();
-                let file = String::from(filestr);
-                v.push(file);
-            }
-            ffi::UnloadDroppedFiles(dropfiles)
-        }
-        v
-    }
     /// Get the directory of the running application.
     pub fn application_directory(&self) -> String {
         unsafe {
@@ -59,5 +74,36 @@ impl RaylibHandle {
     {
         let c_str = CString::new(filename.into())?;
         unsafe { Ok(ffi::IsPathFile(c_str.as_ptr())) }
+    }
+
+    /// Load directory filepaths
+    pub fn load_directory_files(&self, dir_path: OsString) -> FilePathList {
+        unsafe {
+            let c_str = CString::new(dir_path.to_string_lossy().as_bytes()).unwrap(); // .unwrap() is okay here because any nul bytes placed into the actual string should be cleared out by to_string_lossy.
+            FilePathList(ffi::LoadDirectoryFiles(c_str.as_ptr()))
+        }
+    }
+
+    /// Load directory filepaths with extension filtering and recursive directory scan
+    pub fn load_directory_files_ex(
+        &self,
+        dir_path: OsString,
+        filter: String,
+        scan_sub_dirs: bool,
+    ) -> FilePathList {
+        unsafe {
+            let dir_c_str = CString::new(dir_path.to_string_lossy().as_bytes()).unwrap(); // .unwrap() is okay here because any nul bytes placed into the actual string should be cleared out by to_string_lossy.
+            let filter_c_str = CString::new(filter.replace("\0", "").as_bytes()).unwrap();
+            FilePathList(ffi::LoadDirectoryFilesEx(
+                dir_c_str.as_ptr(),
+                filter_c_str.as_ptr(),
+                scan_sub_dirs,
+            ))
+        }
+    }
+
+    /// Check if a file has been dropped into window
+    pub fn load_dropped_files(&self) -> DroppedFilePathList {
+        unsafe { DroppedFilePathList(ffi::LoadDroppedFiles()) }
     }
 }
