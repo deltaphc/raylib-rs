4e86eb4ea8c1c9f4946e677a156cd6662ab14b6b
Dacode45 <davidrolandayeke@gmail.com>
2024-02-06 22:03:39 -0800

    Fixes samples and moves to example directory

--- raylib/examples/samples/model_shader.rs
+++ raylib/examples/samples/model_shader.rs
@@ -15,7 +15,6 @@ fn main() {
         45.0,                         // FOV
     );
 
-    rl.set_camera_mode(&camera, CameraMode::CAMERA_FREE);
     rl.set_target_fps(60);
 
     // Load shader
@@ -50,7 +49,7 @@ fn main() {
     let model_position = Vector3::new(0.0, 0.0, 0.0);
 
     while !rl.window_should_close() {
-        rl.update_camera(&mut camera);
+        rl.update_camera(&mut camera, CameraMode::CAMERA_FREE);
 
         let mut drawing = rl.begin_drawing(&thread);
         drawing.clear_background(Color::WHITE);
